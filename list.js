9461a3351e07ba18a0adfc4697b2dce1349d0c2c
Jacques Uys <jaxuys@gmail.com>
2016-05-03 20:44:16 +0200

    append, remove and find methods added

--- list.js
+++ list.js
@@ -6,6 +6,29 @@ class List {
   length() {
     return this._dataStore.length;
   }
+  append(val) {
+    this._dataStore[this._listSize++] = val;
+  }
+  clear() {
+    this._dataStore = [];
+  }
+  find(val) {
+    for (var i = 0; i < this._dataStore.length; i++) {
+      if (val === this._dataStore[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  remove(val) {
+    let pos = this.find(val);
+    if (pos > -1) {
+      this._dataStore.splice(pos, 1);
+      this._listSize--;
+      return true;
+    }
+    return false;
+  }
 }
 
 var list = new List();
