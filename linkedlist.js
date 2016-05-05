6673a11a4dd79e723c1ef265e1f12fe44183600f
Jacques Uys <jaxuys@gmail.com>
2016-05-05 21:37:46 +0200

    more versatile linkedlist

--- linkedlist.js
+++ linkedlist.js
@@ -7,37 +7,33 @@ class Node {
 
 class LinkedList {
   constructor() {
-    this.head = null;
+    this.head = new Node('head');
     this.tail = null;
   }
-  insert(val) {
-    let node = new Node(val);
-    if (!this.head) {
-      this.head = node;
-    }
-    // point the last node (tail) to the new node
-    if (this.tail) {
-      this.tail.next = node;
-    }
-    // the new node becomes the tail
-    this.tail = node;
-  }
-  removeHead() {
-    if (!this.head) {
-      return null;
-    }
-    let removed = this.head;
-    this.head = this.head.next;
-    return removed.value;
-  }
-  contains(val) {
+  find(target) {
     let current = this.head;
-    while (current) {
-      if (current.value == val) {
-        return true;
-      }
+    while(current.value != target) {
       current = current.next;
     }
-    return false;
+    return current;
+  }
+  findPrevious(target) {
+    let current = this.head;
+    while(current.next.value != target) {
+      current = current.next;
+    }
+    return current;
+  }
+  insert(val, target) {
+    let node = new Node(val);
+    let existing = this.find(target);
+    node.next = existing.next;
+    existing.next = node;
+  }
+  remove(target) {
+    let prev = this.findPrevious(target);
+    if (prev.next !== null) {
+      prev.next = prev.next.next;
+    }
   }
 }
