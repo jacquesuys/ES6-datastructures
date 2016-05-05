8280728f08b2848188bb11d285cafddf585f6365
Jacques Uys <jaxuys@gmail.com>
2016-05-05 11:03:31 +0200

    no underscores

--- list.js
+++ list.js
@@ -1,22 +1,22 @@
 class List {
   constructor() {
-    this._pos = 0;
-    this._listSize = 0;
-    this._dataStore = [];
+    this.pos = 0;
+    this.listSize = 0;
+    this.dataStore = [];
   }
   length() {
-    return this._dataStore.length;
+    return this.dataStore.length;
   }
   append(val) {
-    this._dataStore[this._listSize++] = val;
+    this.dataStore[this.listSize++] = val;
   }
   clear() {
-    this._dataStore = [];
-    this._listSize = 0;
+    this.dataStore = [];
+    this.listSize = 0;
   }
   find(val) {
-    for (var i = 0; i < this._dataStore.length; i++) {
-      if (val === this._dataStore[i]) {
+    for (var i = 0; i < this.dataStore.length; i++) {
+      if (val === this.dataStore[i]) {
         return i;
       }
     }
@@ -25,48 +25,48 @@ class List {
   remove(val) {
     let pos = this.find(val);
     if (pos > -1) {
-      this._dataStore.splice(pos, 1);
-      this._listSize--;
+      this.dataStore.splice(pos, 1);
+      this.listSize--;
       return true;
     }
     return false;
   }
   toString() {
-    return this._dataStore;
+    return this.dataStore;
   }
   insert(val, pos) { 
     let insertPos = this.find(pos); 
     if (insertPos > -1) {
-      this._dataStore.splice(insertPos + 1, 0, val);
-      ++this._listSize;
+      this.dataStore.splice(insertPos + 1, 0, val);
+      ++this.listSize;
       return true;
     }
     return false; 
   }
   // traversal
   front() {
-    this._pos = 0;
+    this.pos = 0;
   }
   end() {
-    this._pos = this._listSize - 1;
+    this.pos = this.listSize - 1;
   }
   prev() {
-    if (this._pos > 0) {
-      --this._pos;
+    if (this.pos > 0) {
+      --this.pos;
     }
   }
   next() {
-    if (this._pos < this._listSize - 1) {
-      ++this._pos;
+    if (this.pos < this.listSize - 1) {
+      ++this.pos;
     }
   }
   currPos() {
-    return this._pos;
+    return this.pos;
   }
   moveTo(pos) {
-    this._pos = pos;
+    this.pos = pos;
   }
   getElement() {
-    return this._dataStore[this._pos];
+    return this.dataStore[this.pos];
   }
 }
