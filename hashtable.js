7edf9104a132ed890b5be38af01ec2d93b820ec3
Jacques Uys <jaxuys@gmail.com>
2016-05-08 22:10:46 +0200

    mvp hashTable

---
+++ hashtable.js
@@ -0,0 +1,23 @@
+class HashTable {
+  constructor() {
+    this.hashTable = new Array(137);
+  }
+  put(val){
+    let hash = this.hashing(val);
+    this.hashTable[hash] = val;
+  }
+  hashing(data) {
+    let total = 0;
+    for(let i = 0; i < data.length; i++) {
+      total += data.charCodeAt(i);
+    }
+    return total % this.hashTable.length;
+  }
+  show(){
+    for (let i = 0; i < this.hashTable.length; i++) {
+      if (this.hashTable[i]) {
+        console.log(i + ' : ' + this.keyhashTable[key]);
+      }
+    }
+  }
+}
