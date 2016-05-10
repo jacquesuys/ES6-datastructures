9fd637863623e77206b81adb52d6c977f4f25861
Jacques Uys <jaxuys@gmail.com>
2016-05-10 19:14:45 +0200

    intersect, union & subset methods

--- set.js
+++ set.js
@@ -3,7 +3,7 @@ class Set {
     this.dataStore = [];
   }
   add(data) {
-    if (this.dataStore.indexOf(data) < 0) {
+    if (!this.contains(data)) {
       this.dataStore.push(data);
       return true;
     }
@@ -19,5 +19,46 @@ class Set {
   }
   show() {
     return this.dataStore;
+  }
+  contains(data) {
+    if (this.dataStore.indexOf(data) > -1) {
+      return true;
+    }
+    return false;
+  }
+  union(set) {
+    let result = new Set();
+    for (let i = 0; i < this.dataStore.length; i++) {
+      result.add(this.dataStore[i]);
+    }
+    for (let i = 0; i < set.dataStore.length; i++) {
+      if (!result.contains(set.dataStore[i])) {
+        result.add(set.dataStore[i]);
+      }
+    }
+    return result;
+  }
+  intersect(set) {
+    let result = new Set();
+    for (let i = 0; i < this.dataStore.length; i++) {
+      if (set.contains(this.dataStore[i])) {
+        result.add(this.dataStore[i]);
+      }
+    }
+    return result;
+  }
+  subset(set) {
+    if (this.size() > set.size()) {
+      return false;
+    }
+    for (let member in this.dataStore) {
+      if(!set.contains(member)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  size() {
+    return this.dataStore.length;
   }
 }
